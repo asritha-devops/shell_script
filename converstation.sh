af7162fa8c69e2f87c78793826af2afb7fcd0c11
asritha-devops <asrithasiri320@gmail.com>
2026-06-23 07:23:26 +0530

    2nd commit

--- converstation.sh
+++ converstation.sh
@@ -1,4 +1,4 @@
-asritha echo "Hi siri"
-siri echo "hi asritha"
-asritha echo "how are you"
-siri echo "i am fine what about you"
+echo "asritha: Hi siri"
+echo "siri: hi asritha"
+echo "asritha: how are you"
+echo "siri: i am fine what about you"

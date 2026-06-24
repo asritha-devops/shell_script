36a9270bd3f03113ce9a61f08947e0b89a02ce0c
asritha-devops <asrithasiri320@gmail.com>
2026-06-24 06:08:31 +0530

    practicing

--- array.sh
+++ array.sh
@@ -3,5 +3,5 @@ FRUIT=("Mango" "Cherry" "Water melon")
 
 echo "I like to eat this ${FRUIT[2]} in summer"
 echo "my faverote fruit is ${FRUIT[0]}"
-echo  "I want to eat this $FRUIT[1] today"
-echo "Alll this fruots are my faverote: ${FRUIT[@]}"
+echo  "I want to eat this ${FRUIT[1]} today"
+echo "All this fruits are my faverote: ${FRUIT[@]}"
